304f14f86ce862caebe7daac073bddb0cdc527c7
Hackbright Student <no-reply@hackbrightacademy.com>
2018-05-09 18:48:32 -0700

    finished scraping data

--- beautiful_soup_parse.py
+++ beautiful_soup_parse.py
@@ -1,13 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+#from model import EIS_data
 
 #create csv file to save data
 #pass in csv to writer method
 #create column names in csv
-# csv_file = open('epa_scrape.csv', 'write')
-# csv_writer = csv.writer(csv_file)
-# csv_writer.writerow(['title','link','federal register date', 'agency', 'state', 'download docs'])
+csv_file = open('epa_scrape.csv', 'write')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Title', 'Document', 'EPA Comment Letter Date', 'Federal Register Date', 'Agency', 'State', 'Download Documents'])
 
 #get request EIS data from EPA and bind to page variable
 page = requests.get("https://cdxnodengn.epa.gov/cdx-enepa-II/public/action/eis/search;jsessionid=44C7A9270B513A7577A50349737B3D3A?d-446779-p=1&d-446779-o=2&search=&d-446779-s=1&commonSearch=openComment#results")
@@ -18,13 +19,15 @@ soup = BeautifulSoup(page.content, 'html.parser')
 #find all information for table
 table = soup.find('table', class_='responsive-table')
 
+rows = table.find('tbody').findAll('tr')
+
 # #save data by column; nest in for loop
 #     title =
 #     link =
 #     fed_reg_date =
 #     agency =
-#     state = 
-#     downloads = 
+#     state =
+#     downloads =
 
 
 #     #add values to csv file by unpacking values into columns; nest in for loop
@@ -40,11 +43,21 @@ table = soup.find('table', class_='responsive-table')
 
 #for i in table.children:
     #print i
-for link in table.find_all('a'):
-    print link.get('href')
+# all_links = []
 
+for row in rows:
+    columns = row.findAll('td')
 
+    title = columns[0].get_text().strip()
+    title_link = columns[0].find('href')
+    document = columns[1].get_text().strip()
+    epa_comment_date = columns[2].get_text().strip()
+    fed_reg_date = columns[3].get_text().strip()
+    agency = columns[4].get_text().strip()
+    state = columns[5].get_text().strip()
+    download_docs = columns[6].get_text().strip()
+    download_docs_link = columns[6].find('href')
 
+    csv_writer.writerow([title, title_link, document, epa_comment_date, fed_reg_date, agency, state, download_docs, download_docs_link])
 
-
-
+csv_file.close()
