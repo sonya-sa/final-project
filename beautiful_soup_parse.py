8b403dd519c8bbd73e5b5515c683953ba9ce1e4c
Sonya Salimy <sonya.salimy@gmail.com>
2018-05-13 18:13:39 -0700

    Updated beautiful_soup_parse file to include href; previous code did not capture link

--- beautiful_soup_parse.py
+++ beautiful_soup_parse.py
@@ -8,7 +8,7 @@ import csv
 #create column names in csv
 csv_file = open('epa_scrape.csv', 'write')
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['Title', 'Document', 'EPA Comment Letter Date', 'Federal Register Date', 'Agency', 'State', 'Download Documents'])
+csv_writer.writerow(['Title', 'Title Link', 'Document', 'EPA Comment Letter Date', 'Federal Register Date', 'Agency', 'State', 'Download Documents', 'Document Link'])
 
 #get request EIS data from EPA and bind to page variable
 page = requests.get("https://cdxnodengn.epa.gov/cdx-enepa-II/public/action/eis/search;jsessionid=44C7A9270B513A7577A50349737B3D3A?d-446779-p=1&d-446779-o=2&search=&d-446779-s=1&commonSearch=openComment#results")
@@ -21,50 +21,37 @@ table = soup.find('table', class_='responsive-table')
 
 rows = table.find('tbody').findAll('tr')
 
-# #save data by column; nest in for loop
-#     title =
-#     link =
-#     fed_reg_date =
-#     agency =
-#     state =
-#     downloads =
-
-
 #     #add values to csv file by unpacking values into columns; nest in for loop
 #     csv_writer.writerow([title, link, fed_reg_date, agency, state, downloads])
 
 # csv_file.close()
 
 #prints EIS database table, including href
-print table.prettify()
+#print table.prettify()
 #print table.get_text()
 #for i in table.descendants:
     #print i
 
 #for i in table.children:
     #print i
-<<<<<<< HEAD
+#<<<<<<< HEAD
 #for link in table.find_all('a'):
     #print link.get('href')
 
-
-=======
-# all_links = []
->>>>>>> 304f14f86ce862caebe7daac073bddb0cdc527c7
 
 for row in rows:
     columns = row.findAll('td')
 
     title = columns[0].get_text().strip()
-    title_link = columns[0].find('href')
+    title_link = columns[0].find('a').get('href')
     document = columns[1].get_text().strip()
     epa_comment_date = columns[2].get_text().strip()
     fed_reg_date = columns[3].get_text().strip()
     agency = columns[4].get_text().strip()
     state = columns[5].get_text().strip()
     download_docs = columns[6].get_text().strip()
-    download_docs_link = columns[6].find('href')
+    download_docs_link = columns[6].find('a').get('href')
 
-    csv_writer.writerow([title, title_link, document, epa_comment_date, fed_reg_date, agency, state, download_docs, download_docs_link])
+    csv_writer.writerow([title, 'https://cdxnodengn.epa.gov' + title_link, document, epa_comment_date, fed_reg_date, agency, state, download_docs, 'https://cdxnodengn.epa.gov' + download_docs_link])
 
 csv_file.close()
