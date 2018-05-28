2a86664a0465743a54fa5db9513d5ab14163c3df
Sonya Salimy <sonya.salimy@gmail.com>
2018-05-28 11:38:13 -0700

    testing server; 1 failure

--- test_server.py
+++ test_server.py
@@ -1,7 +1,20 @@
-class MyTest(unittest.TestCase):
-  def test_home(self):
-      client = server.app.test_client()
+import server
+import unittest
+from unittest import TestCase
+#from model import db, connect_to_db, EIS_data, State, Project_State
+from server import app
+
+class TestFlaskRoutes(unittest.TestCase):
+
+  def test_index(self):
+
+      client = app.test_client(self)
       server.app.config['TESTING'] = True
 
       result = client.get('/')
       self.assertEqual(result.status_code, 200)
+
+if __name__ == '__main__':  #pragma: no cover
+    
+    #runs all cases
+    unittest.main() 
